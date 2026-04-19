1f46b4d6dd965fadf308f45ecc5ec4802f9a127b
Abel Brasil <abelbrasil88@gmail.com>
2026-04-19 20:18:44 -0300

    incluir pasta .streamlit com as senhas para o streamlit cloud

--- app/utils/cache.py
+++ app/utils/cache.py
@@ -4,13 +4,40 @@ from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
 
+# ==============================
+# LOAD .env (LOCAL)
+# ==============================
+
 load_dotenv()
+
+# ==============================
+# FUNÇÃO DE CONEXÃO
+# ==============================
 
 @st.cache_resource
 def get_engine():
-    return create_engine(os.getenv("DATABASE_URL"), pool_pre_ping=True)
+
+    # prioridade: .env (local) → secrets (cloud)
+    db_url = os.getenv("DATABASE_URL")
+
+    if not db_url:
+        try:
+            db_url = st.secrets["DATABASE_URL"]
+        except Exception:
+            raise ValueError("DATABASE_URL não encontrada (.env ou secrets.toml)")
+
+    return create_engine(
+        db_url,
+        pool_pre_ping=True
+    )
+
+# ==============================
+# EXECUÇÃO DE QUERY COM CACHE
+# ==============================
 
 @st.cache_data(ttl=600)
 def run_query(query):
+
     engine = get_engine()
+
     return pd.read_sql(query, engine)
